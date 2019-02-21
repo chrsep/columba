f38f1344fce25e887573217b6b0b37943cfeafc6
Chrisando Pramudhita <chrisep8@gmail.com>
2019-02-21 15:02:29 +0700

    add dummy test, make constant response for testing

--- function.go
+++ function.go
@@ -4,23 +4,32 @@ package p
 import (
 	"encoding/json"
 	"fmt"
-	"html"
 	"net/http"
 )
+
+type CarrierResponse struct {
+	ServiceName string `json:"service_name"`
+	ServiceCode string `json:"service_code"`
+	TotalPrice  string `json:"total_price"`
+	Description string `json:"description"`
+	Currency    string `json:"currency"`
+}
+
+type Respone struct {
+	Rates []CarrierResponse `json:"rates"`
+}
 
 // HelloWorld prints the JSON encoded "message" field in the body
 // of the request or "Hello, World!" if there isn't one.
 func Calculate(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Message string `json:"message"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello World!")
-		return
-	}
-	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
-		return
-	}
-	fmt.Fprint(w, html.EscapeString(d.Message))
+	response := Respone{[]CarrierResponse{{
+		"jne",
+		"jne-1",
+		"20000",
+		"kurir jne",
+		"IDR",
+	}}}
+	js, _ := json.Marshal(response)
+	w.Header().Add("Content-Type", "application/json")
+	fmt.Fprint(w, string(js))
 }
---
+++ function_test.go
@@ -0,0 +1,34 @@
+package p
+
+import (
+	"io/ioutil"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestCalculate(t *testing.T) {
+	tests := []struct {
+		body string
+		want string
+	}{
+		{body: `{"message": ""}`, want: "Hello World!"},
+		{body: `{"message": "Gopher"}`, want: "Hello Gopher!"},
+	}
+
+	for _, test := range tests {
+		req := httptest.NewRequest("GET", "/", strings.NewReader(test.body))
+		req.Header.Add("Content-Type", "application/json")
+
+		rr := httptest.NewRecorder()
+		Calculate(rr, req)
+
+		out, err := ioutil.ReadAll(rr.Result().Body)
+		if err != nil {
+			t.Fatalf("ReadAll: %v", err)
+		}
+		if got := string(out); got != test.want {
+			t.Errorf("HelloHTTP(%q) = %q, want %q", test.body, got, test.want)
+		}
+	}
+}
